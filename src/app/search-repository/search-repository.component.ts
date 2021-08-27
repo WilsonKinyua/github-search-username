7abd087023ddabb5278a66eaa70dd4066e025b52
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-08-27 23:25:48 +0300

    displaying user repositories done

--- src/app/search-repository/search-repository.component.ts
+++ src/app/search-repository/search-repository.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ResponseDataService } from '../services/response-data.service';
 
 @Component({
   selector: 'app-search-repository',
   templateUrl: './search-repository.component.html',
-  styleUrls: ['./search-repository.component.css']
+  styleUrls: ['./search-repository.component.css'],
 })
 export class SearchRepositoryComponent implements OnInit {
+  // accessing the form inputs
+  @ViewChild('f') searchRepoForm: NgForm;
+  userRepositories;
+  searchText: string;
+  displayUserRepositoryList = false;
+  displayUserErrorMessage = false;
 
-  constructor() { }
+  constructor(private userservice: ResponseDataService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // search github user repositories
+  searchGithubUserRepositories() {
+    this.searchText = this.searchRepoForm.value.search;
+    this.userservice.getUserRepositoryRequest(this.searchText).then(
+      (response) => {
+        this.userRepositories = this.userservice.userRepositories;
+        this.displayUserRepositoryList = true;
+      },
+      (error) => {
+        this.displayUserErrorMessage = true;
+        console.log(error);
+      }
+    ); // end of searchGithubUserRepositories
   }
-
 }
