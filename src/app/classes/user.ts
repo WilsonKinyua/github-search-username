660bb6dfe32bcbe27f1257fa7fc749cf40b8f157
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-08-27 10:58:17 +0300

    user properties added

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,2 +1,16 @@
 export class User {
+  constructor(
+    public name: string,
+    public avatar: string,
+    public blog: string,
+    public github_html_link: string,
+    public location: string,
+    public bio: string,
+    public twitter_username: string,
+    public public_repos: number,
+    public followers: number,
+    public following: number,
+    public created_at: Date,
+    public company?: string,
+  ) {}
 }
